862a426ac6ef83d992d0bffc042e5bb00893ec68
Kars1996 <79771285+Kars1996@users.noreply.github.com>
2024-03-01 17:47:27 +0000

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+/*
+Copyright © 2024 Kars (github.com/kars1996)
+
+Not to be shared, replicated or used without prior consent.
+Contact Kars for any enquieries
+*/
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center">
+      <h1>Kars Template</h1>
+      <h2>Edit This file at Page.tsx</h2>
+    </main>
+  );
+}
